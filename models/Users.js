c704a8cd23aff7d6bc2448c90727acca5c382dfd
Raushan7667 <kumarraushan.aic@gmail.com>
2025-03-09 07:36:44 +0530

    User account type

--- models/Users.js
+++ models/Users.js
@@ -24,7 +24,7 @@ const userSchema=new mongoose.Schema({
     },
     accountType:{
         type:String,
-        default:'user',
+        default:'User',
         required:true,
     },
     additionalDetails:{

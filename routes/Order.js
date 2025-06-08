fcd4f5f851cc669f6f8cf41ced19f52a9c1e0faf
Raushan7667 <kumarraushan.aic@gmail.com>
2025-06-08 08:40:02 +0530

    added route for multiple deller

--- routes/Order.js
+++ routes/Order.js
@@ -11,17 +11,17 @@ const { auth,
     isSeller} = require('../middleware/auth');
 const {  updatePaymentInformations, createPaymentLinkBeforeOrder, handleWebhook, verifyPayment} = require('../controller/Payment');
 
-router.post('/createorder/', auth, isUser, createOrder)
-router.post('/createorder/', auth, isUser, createOrder)
-router.get('/findeorderbyid/:orderId',auth,isUser,getOrderById)
-router.get('/orderhistory',auth,isUser,getOrderHistory)
+router.post('/createorder/', auth,  createOrder)
+
+router.get('/findeorderbyid/:orderId',auth,getOrderById)
+router.get('/orderhistory',auth,getOrderHistory)
 router.post('/seller/orders',auth,isSeller,getSellerOrderHistory)
 
 
 
 // router.post('/createpaymentlink/:orderId',auth,isUser,createPaymentLink)
 // router.get('/updatepayemtstatus',auth,isUser,updatePaymentInformations)
-router.post('/create-payment-link-before-order',auth,isUser,createPaymentLinkBeforeOrder)
-router.post("/payment/verify", auth, verifyPayment);
+router.post('/create-payment-link-before-order',auth,createPaymentLinkBeforeOrder)
+router.post('/payment-verify', auth, verifyPayment);
 router.post("/payment/webhook", handleWebhook); 
 module.exports = router;
